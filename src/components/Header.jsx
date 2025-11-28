31d7fecd488df9f9e1ead3a3e850991c225e13aa
AndyJiang <AndyJ0127@users.noreply.github.com>
2025-11-28 11:47:25 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <nav>
+        <Link to="/">首頁</Link>
+        <Link to="/about">關於</Link>
+      </nav>
+    </header>
+  );
+}
 
