ba455cb86306be86501db5570a76ce6c06e83cea
AndyJiang <AndyJ0127@users.noreply.github.com>
2025-11-28 11:45:53 +0800

    Set up React Router with Home, About, and Post routes

--- src/Add.jsx
+++ src/Add.jsx
@@ -1,1 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Post from "./pages/Post";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <Router>
+      <Header/>
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/post/:id" element={<Post/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
