c6682803af3fb9a92c08c9f40a88bccbbf7f9e3b
AndyJiang <AndyJ0127@users.noreply.github.com>
2025-11-28 11:47:11 +0800

    Create Post component with dynamic routing
    Add Post component to display article content based on ID

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,1 +1,13 @@
+import { useParams } from "react-router-dom";
+
+export default function Post() {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h1>文章 #{id}</h1>
+      <p>這是文章內容示範。你可以接上 API 或 CMS 來顯示真正內容。</p>
+    </div>
+  );
+}
 
